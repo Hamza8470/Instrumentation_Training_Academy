729e308df0decf09fddead5d9f18cf00002ff5ea
Hamza8470 <mohammedhamza8470@gmail.com>
2026-02-26 13:24:12 +0530

    Production setup

--- server/src/app.js
+++ server/src/app.js
@@ -18,4 +18,16 @@ app.get('/api/health', (req, res) => {
 app.use('/api/certificates', certificateRoutes);
 app.use('/api/contact', contactRoutes);
 
+const path = require("path");
+
+// Serve frontend static files
+app.use(express.static(path.join(__dirname, "../../client/dist")));
+
+// Only handle non-API routes
+app.get("*", (req, res) => {
+  if (!req.originalUrl.startsWith("/api")) {
+    res.sendFile(path.join(__dirname, "../../client/dist/index.html"));
+  }
+});
+
 module.exports = app;
