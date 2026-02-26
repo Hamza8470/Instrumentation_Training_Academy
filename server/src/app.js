5741c2ada4c9954293392b0b14a4e1e5893c4f2b
Hamza8470 <mohammedhamza8470@gmail.com>
2026-02-26 22:41:18 +0530

    cors

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,8 @@ const app = express();
 app.use(cors({
   origin: [
     process.env.CLIENT_URL || 'http://localhost:5173',
-    "https://ita-patna.vercel.app/"
+    "https://ita-patna.vercel.app/",
+    "https://instrumentation-training-academy.vercel.app/"
   ],
   credentials: true,
 }));
