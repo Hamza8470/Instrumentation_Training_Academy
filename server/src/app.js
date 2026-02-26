4a8e5f690911e0a776cb3471cb36b18846b137e3
Hamza8470 <mohammedhamza8470@gmail.com>
2026-02-26 22:48:30 +0530

    Fix CORS domains properly

--- server/src/app.js
+++ server/src/app.js
@@ -7,12 +7,13 @@ const app = express();
 
 app.use(cors({
   origin: [
-    process.env.CLIENT_URL || 'http://localhost:5173',
-    "https://ita-patna.vercel.app/",
-    "https://instrumentation-training-academy.vercel.app/"
+    "http://localhost:5173",
+    "https://ita-patna.vercel.app",
+    "https://instrumentation-training-academy.vercel.app"
   ],
   credentials: true,
 }));
+
 app.use(express.json());
 
 app.get('/api/health', (req, res) => {
